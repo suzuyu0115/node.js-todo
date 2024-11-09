4d92748f35982bdceed850637e357b85052a877f
suzuyu0115 <szkyut0115@gmail.com>
2024-11-09 20:51:38 +0900

    mongooseでmongodbに接続

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");require("dotenv").config();
 
 const PORT = 8080;
-app.listen(PORT, console.log('サーバー起動中^_^...'));
+
+const start = async () => {
+  try {
+    app.listen(PORT, console.log('サーバー起動中^_^...'));
+    await mongoose.connect(process.env.MONGO_URL)
+    .then(() => { console.log('DB接続中...') });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 const router = require('./src/routes/routes');
 app.use('/api/v1/tasks', router);
+
+start();
