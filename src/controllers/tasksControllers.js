8f171a4ede7bc7485d2b8f0a53cabd083997ae14
suzuyu0115 <szkyut0115@gmail.com>
2024-11-11 00:13:43 +0900

    タスクを全て取得するAPIの作成

--- src/controllers/tasksControllers.js
+++ src/controllers/tasksControllers.js
@@ -1,12 +1,17 @@
 const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-  res.send('タスクを全取得');
+const getAllTasks = async (req, res) => {
+  try {
+    const getAllTasks = await Task.find({});
+    res.status(200).json(getAllTasks);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 
 const createTask = async (req, res) => {
   try {
-    await Task.create(req.body);
+    const createTask = await Task.create(req.body);
     res.status(200).json(createTask);
   } catch (err) {
     res.status(500).json(err);
