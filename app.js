ee0efb36fcdbd74cc1594b5e9f9f539072a2f969
suzuyu0115 <szkyut0115@gmail.com>
2024-11-14 01:32:44 +0900

    index.htmlの読み込みと作成

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");require("dotenv").config();
 
 const PORT = 8080;
 app.use(express.json());
+app.use(express.static('./src/public'));
 
 const start = async () => {
   try {
