ebd4db7265d6a0ff7ef5f671a1cd6003fb8bdf4a
suzuyu0115 <szkyut0115@gmail.com>
2024-11-15 00:40:28 +0900

    全タスクをフロントで表示

--- src/public/script.js
+++ src/public/script.js
@@ -1,1 +1,24 @@
+const tasksDOM = document.querySelector('.tasks');
 
+const showTasks = async () => {
+  try {
+    const { data: tasks } = await axios.get('/api/v1/tasks');
+    console.log(tasks);
+
+    const allTasks = tasks.map((task) => {
+      return `
+        <div class="task">
+          <p>${task.title}</p>
+          <a href="#">編集</a>
+          <button>削除</button>
+        </div>
+      `;
+    }).join("");
+
+    tasksDOM.innerHTML = allTasks;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+showTasks();
