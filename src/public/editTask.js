a7a3b0b3976fccdd64eea7583a35208d24697f3b
suzuyu0115 <szkyut0115@gmail.com>
2024-11-22 22:08:43 +0900

    編集フォームの初期値に元のタスク名を入れる

--- src/public/editTask.js
+++ src/public/editTask.js
@@ -1,4 +1,5 @@
 taskIdDOM = document.querySelector('.task-id');
+taskEditDOM = document.querySelector('.task-title-edit');
 
 const params = location.search;
 const id = params.replace('?id=', '');
@@ -6,8 +7,10 @@ const id = params.replace('?id=', '');
 const showTask = async () => {
   try {
     const { data: task } = await axios.get(`/api/v1/tasks/${id}`);
-    const { _id, completed, name } = task;
+    console.log(task);
+    const { _id, completed, title } = task;
     taskIdDOM.textContent = _id;
+    taskEditDOM.value = title;
   } catch (err) {
     console.log(err);
   }
